6f13ca0464327983e441007fac3b6c635d5125d2
davideerusso <rd.96.rd@gmail.com>
2025-04-08 17:04:31 +0200

    es2

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,13 +1,18 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-
-
+let iniziali = [];
 // Dichiara la funzione qui.
+function search(listaNomi) {
+  for (let i = 0; i < listaNomi.length; i++) {
+    const nomi = listaNomi[i];
+    iniziali.push(nomi[0]);
+    console.log(nomi);
+  }
 
-
+  return iniziali;
+}
+console.log(iniziali);
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
+console.log(search(names));
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
